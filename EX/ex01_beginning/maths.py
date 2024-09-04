3fedb45beeb73c43bb7f8c78471413147279abc1
Emil Kalm <emkalm@taltech.ee>
2024-09-04 16:25:33 +0300

    apu

--- EX/ex01_beginning/maths.py
+++ EX/ex01_beginning/maths.py
@@ -73,7 +73,7 @@ x = int(input("Enter the value of x: "))
 y = int(input("Enter the value of y: "))
 u = int(input("Enter the value of u: "))
 t = int(input("Enter the value of t: "))
-print(x * t + y * u, "/", y * t)
+print(f"{x * t + y * u} / {y * t}")
 
 """
 Calculate and print how many hours are needed per week with given ECTS and amount of weeks, if each ECTS is 26 hours.

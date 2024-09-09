43716c70e625623798873a649b5519e9c3a5e845
Emil Kalm <emkalm@taltech.ee>
2024-09-09 20:23:35 +0300

    less the better

--- EX/ex02_loops/prime.py
+++ EX/ex02_loops/prime.py
@@ -14,9 +14,7 @@ def is_prime_number(number: int) -> bool:
     :param number: the number to check.
     :return: boolean True if number is a prime number or False if number is not a prime number.
     """
-    if number == 2:
-        return True
-    
+
     if number > 1:
         for i in range(2, number):
             if number % i == 0:

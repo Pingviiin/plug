94810e4f2f0927b04a971c76deb95f0c22829c3b
Emil Kalm <emkalm@taltech.ee>
2024-09-09 22:11:23 +0300

    k

--- EX/ex02_loops/caesar.py
+++ EX/ex02_loops/caesar.py
@@ -26,15 +26,15 @@ def encode(message: str, shift: int) -> str:
     """
     ciphered_message = ""
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    big_alphabet = ""
+    if shift != 0:
+        big_alphabet = alphabet
+    else:
+        big_alphabet = alphabet * shift
     for letter in message:
         if letter.isalpha():
             letter_location = (alphabet.find(letter))
-            if letter_location + shift >= len(alphabet):
-                loop_letter = len(alphabet) - (letter_location + shift)
-                letter_location = abs(loop_letter)
-                letter = alphabet[letter_location]
-            else:
-                letter = alphabet[letter_location + shift]
+            letter = big_alphabet[letter_location + shift]
 
         ciphered_message += letter
 
@@ -43,8 +43,8 @@ def encode(message: str, shift: int) -> str:
 
 
 if __name__ == '__main__':
+    #print(encode("don't change", 0))  # -> don't change
     print(encode("o roqk zaxzrky", 20))  # -> i like turtles
     print(encode("i like turtles", 6))  # -> o roqk zaxzrky
     print(encode("example", 1))  # -> fybnqmf
-    print(encode("don't change", 0))  # -> don't change
     print(encode('the quick brown fox jumps over the lazy dog.', 7))  # -> aol xbpjr iyvdu mve qbtwz vcly aol shgf kvn.

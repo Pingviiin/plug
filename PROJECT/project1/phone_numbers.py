baa1aecf17591b4af7ac46987c125e9216b436bf
Emil Kalm <emkalm@taltech.ee>
2024-10-01 16:03:57 +0300

    mina olen väga segaduses rn

--- PROJECT/project1/phone_numbers.py
+++ PROJECT/project1/phone_numbers.py
@@ -43,8 +43,8 @@ def remove_unnecessary_chars(number: str) -> str:
     num = filter(lambda x: x.isdigit(), num)
     num = "".join(num)
 
-    if len(cc) > 5:
-        return num
+    if num == cc:
+        return cc
     
     if num == "":
         return cc

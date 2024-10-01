3fa2b85e067b92b57d39bbed5b12e2b2ccef08d8
Emil Kalm <emkalm@taltech.ee>
2024-10-01 15:48:21 +0300

    ma ei tea enam mida ma täpselt teen aga pray to god et see töötab

--- PROJECT/project1/phone_numbers.py
+++ PROJECT/project1/phone_numbers.py
@@ -29,8 +29,11 @@ def is_valid(number: str) -> str:
 
 def remove_unnecessary_chars(number: str) -> str:
     num = number.rsplit(" ", 1)
-    cc = num[0]
-    num = num[1]
+    
+    if len(cc) > 1:
+        cc = num[0]
+    if len(num) > 1:
+        num = num[1]
 
 
     cc = list(cc)

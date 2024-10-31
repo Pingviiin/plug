77a75a1787efa50ec049be10c3be8e65529dfc9a
Emil Kalm <emkalm@taltech.ee>
2024-10-31 21:34:02 +0200

    this is some bull

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,12 +1,26 @@
 def function_a(x: int) -> int:
-    pass
-def function_b(x):
-    pass
-def function_c(x):
-    pass
-def function_d(x):
-    pass
-def function_e(x):
-    pass
-def function_f(x):
-    pass
+    return "a"
+def function_b(x: int) -> int:
+    return "a"
+def function_c(x: int) -> int:
+    return "a"
+def function_d(x: int) -> int:
+    return "a"
+def function_e(x: int) -> int:
+    return "a"
+def function_f(x: int) -> int:
+    return "a"
+def function_g(x: int) -> int:
+    return "a"
+def function_h(x: int) -> int:
+    return "a"
+def function_i(x: int) -> int:
+    return "a"
+def function_j(x: int) -> int:
+    return "a"
+def function_k(x: int) -> int:
+    return "a"
+def function_l(x: int) -> int:
+    return "a"
+def function_m(x: int) -> int:
+    return "WAT"

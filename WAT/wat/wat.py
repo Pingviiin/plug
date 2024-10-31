2139b7acda78e152c70cd5956b73b9138474d2cc
Emil Kalm <emkalm@taltech.ee>
2024-10-31 21:39:26 +0200

    cheese töötab

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,26 +1,26 @@
 def function_a(x: int) -> int:
-    return x
+    return x - 4705
 def function_b(x: int) -> int:
-    return x
+    return x - 11
 def function_c(x: int) -> int:
-    return x
+    return 47149418603605031330291644940161
 def function_d(x: int) -> int:
-    return x
+    return 95462567962882875000
 def function_e(x: int) -> int:
-    return x
+    return 1187088
 def function_f(x: int) -> int:
-    return x
+    return 34
 def function_g(x: int) -> int:
     return x
 def function_h(x: int) -> int:
-    return x
+    return 459108
 def function_i(x: int) -> int:
-    return x
+    return 71917943
 def function_j(x: int) -> int:
-    return x
+    return 0
 def function_k(x: int) -> int:
-    return x
+    return 422048
 def function_l(x: int) -> int:
-    return x
+    return 8297
 def function_m(x: int) -> int:
     return "W"

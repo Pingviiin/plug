5bb9b775c283d4ec4ec8f48867fab3181b24eeb8
Emil Kalm <emkalm@taltech.ee>
2024-10-31 10:12:37 +0200

    go x men

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,10 +1,12 @@
-def function_a():
+def function_a(x):
     pass
-def function_b():
+def function_b(x):
     pass
-def function_c():
+def function_c(x):
     pass
-def function_d():
+def function_d(x):
     pass
-def function_f():
+def function_e(x):
+    pass
+def function_f(x):
     pass

c981fe72965a3180df93d60d07a8f27a7e8059da
Emil Kalm <emkalm@taltech.ee>
2024-10-31 21:35:34 +0200

    ma saan selle tehtud

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,26 +1,26 @@
 def function_a(x: int) -> int:
-    return "a"
+    return x
 def function_b(x: int) -> int:
-    return "a"
+    return x
 def function_c(x: int) -> int:
-    return "a"
+    return x
 def function_d(x: int) -> int:
-    return "a"
+    return x
 def function_e(x: int) -> int:
-    return "a"
+    return x
 def function_f(x: int) -> int:
-    return "a"
+    return x
 def function_g(x: int) -> int:
-    return "a"
+    return x
 def function_h(x: int) -> int:
-    return "a"
+    return x
 def function_i(x: int) -> int:
-    return "a"
+    return x
 def function_j(x: int) -> int:
-    return "a"
+    return x
 def function_k(x: int) -> int:
-    return "a"
+    return x
 def function_l(x: int) -> int:
-    return "a"
+    return x
 def function_m(x: int) -> int:
-    return "WAT"
+    return "W"

b9dea82fc35787abfdb565284e9b2d4a3e3a1a36
Emil Kalm <emkalm@taltech.ee>
2024-11-06 19:43:52 +0200

    lil checkup

--- PROJECT/project2/pizza_kiosk.py
+++ PROJECT/project2/pizza_kiosk.py
@@ -49,11 +49,40 @@ def fix_names(ingredients: list) -> list:
 
 
 def pizza_at_index(pizzas: list, pizza: str) -> str:
-    pass
+    """Find pizza at index, which is a count of how many of one specific pizza are in the list.
 
+    Args:
+        pizzas (list): List of pizzas
+        pizza (str): Pizza, which will be indexed
+
+    Returns:
+        str: Name of pizza, which is at the index
+    """
+    pizzas = fix_names(pizzas)
+    index = pizzas.count(pizza)
+    if index == 0:
+        return ""
+    if index >= len(pizzas):
+        return ""
+    else:
+        return pizzas[index]
+
+
+def format_orders(nr_order: list) -> dict:
+    output = {}
+    for order in nr_order:
+        order = order.split("&")
+        output[order[0]] = order[1].lower()
+    return output
+        
 print(is_correct_name("banana"))
 print(is_correct_name("Banana"))
 print(is_correct_name("12412Banana"))
 print(is_correct_name("PINEAPPLE--."))
 print(fix_names(["banana", "BANANA", ""]))
 print(fix_names(["2423dsfsdf", ".dfgdgffdg...."]))
+print(pizza_at_index(["pepperoni", "kanapitsa", "juustupitsa"], "juustupitsa"))
+print(pizza_at_index(["pepperoni", "juustupitsa", "kanapitsa", "juustupitsa", "bbq"], "juustupitsa"))
+print(pizza_at_index(["pepperoni"], "pepperoni"))
+print(pizza_at_index(["pepperoni", "kanapitsa", "bbq"], "juustupitsa"))
+print(format_orders(["5&kanapitsa", "1&pepperoni", "20&MeXican"]))

bee84ebfbdcfeadd8ae8b5b04e39b1dbb4a2ddc3
Emil Kalm <emkalm@taltech.ee>
2024-11-06 21:19:14 +0200

    wtf is going on

--- PROJECT/project2/pizza_kiosk.py
+++ PROJECT/project2/pizza_kiosk.py
@@ -1,3 +1,5 @@
+import re
+
 def is_correct_name(ingredient: str) -> bool:
     """Check if ingredient name is correct.
 
@@ -74,7 +76,42 @@ def format_orders(nr_order: list) -> dict:
         order = order.split("&")
         output[order[0]] = order[1].lower()
     return output
-        
+
+
+def calculate_income(prices: str) -> float:
+    prices = re.findall(r'(\d{2}\.\d{2})', prices)
+
+    def price_total(prices, total):
+        if not prices:
+            return total
+        else:
+            return price_total(prices[1:], total + float(prices[0]))
+
+    return price_total(prices, 0.0)
+
+
+def switch_keys_and_values(pizza_orders: dict) -> dict:
+    output = {}
+    for pizza, nums in pizza_orders.items():
+        for i in nums:
+            if i not in output:
+                output[i] = [pizza]
+            else:
+                output[i] += [pizza]
+    return output
+
+
+def count_ingredients(menu: dict, order: list) -> dict | None:
+    output = {}
+    for pizza in order:
+        for ingredient in menu[pizza]:
+            if ingredient not in output:
+                output[ingredient] = 1
+            else:
+                output[ingredient] += 1
+    return dict(sorted(output.items(), key=lambda i: i[1], reverse=True))
+
+
 print(is_correct_name("banana"))
 print(is_correct_name("Banana"))
 print(is_correct_name("12412Banana"))
@@ -86,3 +123,7 @@ print(pizza_at_index(["pepperoni", "juustupitsa", "kanapitsa", "juustupitsa", "b
 print(pizza_at_index(["pepperoni"], "pepperoni"))
 print(pizza_at_index(["pepperoni", "kanapitsa", "bbq"], "juustupitsa"))
 print(format_orders(["5&kanapitsa", "1&pepperoni", "20&MeXican"]))
+print(calculate_income("15.03*05.99|)=01.20&.$50.37"))
+print(calculate_income(""))
+print(switch_keys_and_values({"kanapitsa": [1, 5, 3, 4], "juustupitsa": [1, 2], "pepperoni": [1, 5, 3]}))
+print(count_ingredients({"margarita": ["juust", "tomat", "kaste"], "pepperoni": ["juust", "kaste", "pepperoni"]}, ["margarita", "margarita", "pepperoni"]))

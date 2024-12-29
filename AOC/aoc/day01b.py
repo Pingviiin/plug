3f028505c386ddca8cc9cc0c567515a263048517
Emil Kalm <emkalm@taltech.ee>
2024-12-29 20:05:29 +0200

    nüüd on eraldi failid

---
+++ AOC/aoc/day01b.py
@@ -0,0 +1,11 @@
+"""Häid jõule."""
+from collections import Counter
+
+
+def similarity_score(tuples):
+    # Make a counter map of the right part
+    rights_counts = Counter([t[0] for t in tuples])
+
+    # Sum for everything in left part the number * count
+    score = sum(num * rights_counts[num] for num in [t[1] for t in tuples])
+    return score
